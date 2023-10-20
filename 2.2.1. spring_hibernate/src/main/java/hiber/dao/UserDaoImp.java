aedc0613738dcff4414a86a61b5df8a9b84eecf3
Artem Pronkin <artempro1994@gmail.com>
2023-10-20 18:03:24 +0500

    first commit

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -1,12 +1,15 @@
 package hiber.dao;
 
+
 import hiber.model.User;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-
 import javax.persistence.TypedQuery;
 import java.util.List;
+
 
 @Repository
 public class UserDaoImp implements UserDao {
@@ -20,10 +23,23 @@ public class UserDaoImp implements UserDao {
    }
 
    @Override
+   public User getUserByCar(String model, int series) {
+      Session session = sessionFactory.openSession();
+      String hql = "from User user join fetch user.car car where car.model =:carModel and car.series =: carSeries";
+      User user = session.createQuery(hql, User.class)
+              .setParameter("carModel", model)
+              .setParameter("carSeries", series)
+              .uniqueResult();
+
+      session.close();
+      return user;
+
+   }
+
+   @Override
    @SuppressWarnings("unchecked")
    public List<User> listUsers() {
       TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
       return query.getResultList();
    }
-
 }
